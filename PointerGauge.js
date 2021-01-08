ccd7bfc63c93b19289ebc5a3769ac949a449b3e0
Mesum Raza Hemani <mes.javacca@gmail.com>
2021-01-09 04:24:27 +0500

    Update PointerGauge.js

--- PointerGauge.js
+++ PointerGauge.js
@@ -119,6 +119,7 @@
 		var range = config.max - config.min;
 		config.yellowZones = [{ from: config.min + range*0.75, to: config.min + range*0.9 }];
 		config.redZones = [{ from: config.min + range*0.9, to: config.max }];
+		document.write(value);
 		var gauge= Gauge("fillgauge", config);
 		gauge.redraw(value);
 		
